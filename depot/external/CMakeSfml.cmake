e067d1bc3c1495901c8ee306aea703463b2222bf
Harrison Ting <runhappy@gmail.com>
2012-10-16 02:30:40 -0700

    Build SFML from source

--- depot/external/CMakeSfml.cmake
+++ depot/external/CMakeSfml.cmake
@@ -1,23 +1,28 @@
 set(sfmlDir ${externalDir}/SFML-2.0-rc)
 set(sfmlIncludeDir ${sfmlDir}/include)
-set(sfmlLibDir ${sfmlDir}/lib)
-set(sfmlBinDir ${sfmlDir}/bin)
+set(sfmlBuildDir ${buildDir}/external/SFML-2.0-rc)
+set(sfmlLibDir ${sfmlBuildDir}/lib)
+set(sfmlBinDir ${sfmlBuildDir}/lib)
 
 include_directories(${sfmlIncludeDir})
 
 # Link SFML libs against target and copy the dll.
 function(targetLinkSfmlLibs target)
 	foreach(libName ${ARGN})
-		target_link_libraries(${target}
-			debug ${sfmlLibDir}/sfml-${libName}-d.lib
-			optimized ${sfmlLibDir}/sfml-${libName}.lib)
+		# Use the below when building SFML libraries.
+		target_link_libraries(${target}	sfml-${libName})
+
+		# Use the below when linking against prebuilt SFML libraries.
+		#target_link_libraries(${target}
+		#	debug ${sfmlLibDir}/sfml-${libName}-d.lib
+		#	optimized ${sfmlLibDir}/sfml-${libName}.lib)
 
 		# Copy SFML DLLs.
 		ADD_CUSTOM_COMMAND(TARGET ${target} POST_BUILD
 			COMMAND if [$<CONFIGURATION>]==[Debug] (
-						${CMAKE_COMMAND} -E copy_if_different "${sfmlBinDir}/sfml-${libName}-d-2.dll" "$<TARGET_FILE_DIR:Game>"
+						${CMAKE_COMMAND} -E copy_if_different "${sfmlLibDir}/$<CONFIGURATION>/sfml-${libName}-d-2.dll" "$<TARGET_FILE_DIR:Game>"
 					) else (
-						${CMAKE_COMMAND} -E copy_if_different "${sfmlBinDir}/sfml-${libName}-2.dll" "$<TARGET_FILE_DIR:Game>"
+						${CMAKE_COMMAND} -E copy_if_different "${sfmlLibDir}/$<CONFIGURATION>/sfml-${libName}-2.dll" "$<TARGET_FILE_DIR:Game>"
 					)
 			COMMENT "Copying SFML DLL (${libName})..."
 			VERBATIM)
