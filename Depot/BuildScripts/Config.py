69c361a8f5954a0683590fc4d225ac40c4ea1f5c
Harrison Ting <runhappy@gmail.com>
2012-11-14 02:28:43 -0800

    Finished converting Generate.bat to Generate.py and removed Generate.bat.  Refactored CMake python functionality into CMake.py.

--- Depot/BuildScripts/Config.py
+++ Depot/BuildScripts/Config.py
@@ -1,7 +1,5 @@
+import CMake
+
 SOLUTION_NAME = "Mario"
 
-class CmakeGenerators:
-	VISUAL_STUDIO_11 = "Visual Studio 11"
-	NMAKE_MAKEFILES = "NMake Makefiles"
-
-CMAKE_GENERATOR = CmakeGenerators.VISUAL_STUDIO_11
+CMAKE_GENERATOR = CMake.Generators.VISUAL_STUDIO_11
--- Depot/BuildScripts/Generate.py
+++ Depot/BuildScripts/Generate.py
@@ -1,3 +1,4 @@
+import CMake
 import CommonCommands as Commands
 import CommonDirectories as Directories
 import Config
@@ -6,40 +7,20 @@ import FunctionTiming
 import os
 import subprocess
 import sys
-import winreg
-
-def is_visual_studio_11_installed():
-	return is_visual_studio_11_installed_helper(r"Software\Microsoft\DevDiv\vs\Servicing\11.0") \
-		or is_visual_studio_11_installed_helper(r"Software\Microsoft\DevDiv\vc\Servicing\11.0")
-
-# We expect there to be at least 1 value under vs11_key_path.
-def is_visual_studio_11_installed_helper(vs11_key_path):
-	root_key = winreg.HKEY_LOCAL_MACHINE
-	try:
-		vs11_key = winreg.OpenKey(root_key, vs11_key_path)
-		winreg.EnumValue(vs11_key, 0)
-		return True
-	except OSError:
-		return False
 
 def fail_due_to_missing_generator(generator):
 	sys.exit("The CMake generator is set to '{}' in '{}/Config.py', but it cannot be found.".format(
 		generator,
 		Directories.BUILD_SCRIPTS_DIR))
 
-def validate_cmake_generator(generator):
-	if generator == Config.CmakeGenerators.VISUAL_STUDIO_11:
-		if not is_visual_studio_11_installed():
-			fail_due_to_missing_generator(generator)
-	elif generator == Config.CmakeGenerators.NMAKE_MAKEFILES:
-		# TODO(HTing): actually check for NMake instead of just checking for VS11.
-		if not is_visual_studio_11_installed():
-			fail_due_to_missing_generator(generator)
-	else:
-		sys.exit("Invalid CMake generator '{}'".format(generator))
-
 def run_cmake(generator, build_dir, depot_dir):
-	validate_cmake_generator(generator)
+	generator_validity = CMake.get_cmake_generator_validity(generator)
+	if generator_validity == CMake.GeneratorValidity.INVALID:
+		sys.exit("The CMake generator '{}', set in '{}/Config.py', is invalid.".format(
+			generator, Directories.BUILD_SCRIPTS_DIR))
+	elif generator_validity == CMake.GeneratorValidity.MISSING:
+		sys.exit("The CMake generator '{}', set in '{}/Config.py', cannot be found.".format(
+			generator, Directories.BUILD_SCRIPTS_DIR))
 
 	# Run from the build directory.
 	os.makedirs(build_dir, exist_ok=True)
