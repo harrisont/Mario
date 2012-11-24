807c68dab32d7a4daa00835031a3493a228a1fa1
Harrison Ting <runhappy@gmail.com>
2012-11-24 15:49:40 -0800

    Converted root SetupWorkspace.bat to Depot/BuildScripts/SetupWorkspace.py.  Fixes #2.

--- Depot/BuildScripts/CommonDirectories.py
+++ Depot/BuildScripts/CommonDirectories.py
@@ -10,3 +10,7 @@ TARGET_DIR = ROOT_DIR + "/Target"
 BUILD_SCRIPTS_DIR = DEPOT_DIR + "/BuildScripts"
 SOURCE_DIR = DEPOT_DIR + "/Source"
 EXTERNAL_DIR = DEPOT_DIR + "/External"
+
+GIT_DIR = DEPOT_DIR + "/Git"
+GIT_HOOKS_DEPOT_DIR = GIT_DIR + "/Hooks"
+GIT_HOOKS_ROOT_DIR = ROOT_DIR + "/.git/hooks"
--- SetupWorkspace.bat
+++ SetupWorkspace.bat
@@ -1,3 +1,1 @@
-@call Clean.bat
-@copy /Y Depot\Git\Hooks\* .git\hooks\
-@call GenerateAndBuild.bat
+@call "Depot/External/Python/Python-3.3.0/python.exe" Depot/BuildScripts/SetupWorkspace.py
