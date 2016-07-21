c3983d6252f634b62861fbccacee1194031acd2f
Harrison Ting <runhappy@gmail.com>
2016-07-21 01:14:27 -0700

    Cherry picking SFML commit 6b97814 to add VS2015 support

--- Depot/External/SFML/Version.cmake
+++ Depot/External/SFML/Version.cmake
@@ -1,2 +1,4 @@
-set(sfmlVersion 2.3.2)
+# The latest stable release is 2.3.2, but it doesn't have support for Visual Studio 2015.
+# Cherry pick https://github.com/SFML/SFML/commit/6b97814 to add VS2015 support.
+set(sfmlVersion 2.3.2+6b97814)
 set(sfmlVersionDir ${sfmlDir}/v${sfmlVersion})
