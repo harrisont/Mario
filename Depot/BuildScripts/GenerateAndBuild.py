04d85683a93d387078abfaaf571b3901dd1d67c0
Harrison Ting <runhappy@gmail.com>
2016-07-20 22:56:23 -0700

    Fix PEP-8 errors

--- Depot/BuildScripts/GenerateAndBuild.py
+++ Depot/BuildScripts/GenerateAndBuild.py
@@ -2,23 +2,29 @@ import Build
 import FunctionTiming
 import Generate
 
+
 def generate_and_build():
-	""" Returns 0 on success, non-0 on failure.
-	"""
-	generate_result = Generate.generate()
-	if generate_result != 0:
-		print("Generate failed with return value '{}'".format(generate_result))
-		return generate_result
+    """ Returns 0 on success, non-0 on failure.
+    """
+    generate_result = Generate.generate()
+    if generate_result != 0:
+        print("Generate failed with return value '{}'".format(generate_result))
+        return generate_result
 
-	build_result = Build.build()
-	if build_result != 0:
-		print("Build failed with return value '{}'".format(build_result))
-		return build_result
+    build_result = Build.build()
+    if build_result != 0:
+        print("Build failed with return value '{}'".format(build_result))
+        return build_result
 
-	return 0
+    return 0
+
+
+def main():
+    result, duration = FunctionTiming.time_function(generate_and_build)
+    duration_str = FunctionTiming.get_duration_str(duration)
+    print("Generate and build duration: {}".format(duration_str))
+    exit(result)
+
 
 if __name__ == "__main__":
-	result, duration = FunctionTiming.time_function(generate_and_build)
-	duration_str = FunctionTiming.get_duration_str(duration)
-	print("Generate and build duration: {}".format(duration_str))
-	exit(result)
+    main()
